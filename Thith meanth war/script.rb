0ae9f1f680c1f8e438895306144692fdd20e2145
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-04-12 18:03:46 +0200

    adding comments...

--- Thith meanth war/script.rb
+++ Thith meanth war/script.rb
@@ -1,6 +1,8 @@
 print "Write something down :"
 user_input = gets.chomp
 user_input.downcase!
+
+# looking for "S" in the sentence then swaping it with "TH"
 if user_input.include? "s"
     user_input.gsub!(/s/, "th")
     puts "your sentence is #{user_input}"
