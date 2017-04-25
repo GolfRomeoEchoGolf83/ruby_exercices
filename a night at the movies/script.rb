5cd7e9f2129659361349180fab41cde0d7ccd213
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-04-25 18:53:42 +0200

    updating the database dealing with empty key/value

--- a night at the movies/script.rb
+++ a night at the movies/script.rb
@@ -13,21 +13,37 @@ puts "-- Type 'delete' to delete a movie."
 choice = gets.chomp
 
 case choice 
+
 # adding a movie
 when "add"
 puts "Add a movie title"
 title = gets.chomp.downcase
+
 #test if the movie has aleady been added to the DB
 if movies[title.to_sym].nil? 
     puts "Add a rating from 1 to 5"
     rating = gets.chomp
     movies[title.to_sym] = rating.to_i
     puts "You added #{title.capitalize} with a rating of #{rating}"
-    else 
-        puts "Your movie has already been added."
-    end
+ else 
+     puts "Your movie has already been added."
+ end
+
 # updating a movie
 when "update"
+puts "what movie do you want to update ?"
+title = gets.chomp.downcase
+
+# test if the movie already exist
+if movies[title.to_sym].nil?
+    puts "The movie you are looking for does not exist"
+else 
+    puts "Update the movie's rating"
+    rating = gets.chomp
+    movies[title.to_sym] = rating.to_i
+    puts "#{title} has been updated with the new rating #{rating}"
+end
+
 # display the DB
 when "display"
 # delete a movie
