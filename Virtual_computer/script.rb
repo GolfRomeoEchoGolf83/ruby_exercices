1c88853f859bc39435e2087ac59d34518e52cc47
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-04-28 16:09:20 +0200

    Getting class variable @@users with class method to get user name

--- Virtual_computer/script.rb
+++ Virtual_computer/script.rb
@@ -7,11 +7,17 @@ class Computer
         @files = {}
         @@users[username] = password
     end
+
     # Create method for creating file with its time of creation and name
      def create(filename)
         @time = Time.now
         @files[filename] = time
         puts "#{username} has created a file named : #{filename} at #{time}"
+    end
+
+    # Getting class variable @@users with class method to get user name 
+    def Computer.get_users 
+        return @@users
     end
 end
 
