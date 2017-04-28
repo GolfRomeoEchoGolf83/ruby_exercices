47f93a167bea553e6c4857c8e4d305625564786c
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-04-28 16:08:06 +0200

    Method for creating file with its time of creation and nam

--- Virtual_computer/script.rb
+++ Virtual_computer/script.rb
@@ -1,9 +1,17 @@
 class Computer
     @@users = {}
+    # Initialize class
     def initialize(username, password)
         @username = username
         @password = password
         @files = {}
         @@users[username] = password
     end
+    # Create method for creating file with its time of creation and name
+     def create(filename)
+        @time = Time.now
+        @files[filename] = time
+        puts "#{username} has created a file named : #{filename} at #{time}"
+    end
 end
+
